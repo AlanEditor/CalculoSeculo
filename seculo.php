b0240af3517bb7fd98f3c524ef91202f9c6e4d07
Alan <alansantos04@hotmail.com>
2022-05-02 20:24:30 -0300

    Mudando o valor de Romanos para Decimal

--- seculo.php
+++ seculo.php
@@ -17,26 +17,27 @@ echo seculoAno($ano);
 
 function seculoAno($ano)
 {
-   if($ano <= 100)  return 'Século I';
-   if($ano <= 200)  return 'Século II';
-   if($ano <= 300)  return 'Século III';
-   if($ano <= 400)  return 'Século IV';
-   if($ano <= 500)  return 'Século V';
-   if($ano <= 600)  return 'Século IV';
-   if($ano <= 700)  return 'Século VII';
-   if($ano <= 800)  return 'Século VIII';
-   if($ano <= 900)  return 'Século IX';
-   if($ano <= 1100) return 'Século XI';
-   if($ano <= 1200) return 'Século XII';
-   if($ano <= 1300) return 'Século XII';
-   if($ano <= 1400) return 'Século XIV';
-   if($ano <= 1500) return 'Século XV';
-   if($ano <= 1600) return 'Século XVI';
-   if($ano <= 1700) return 'Século XVII';
-   if($ano <= 1800) return 'Século XVII';
-   if($ano <= 1900) return 'Século XIX';
-   if($ano <= 2000) return 'Século XX';
-   if($ano <= 2022) return 'Século XXI'; 
+   if($ano <= 100)  return 'Século 1';
+   if($ano <= 200)  return 'Século 2';
+   if($ano <= 300)  return 'Século 3';
+   if($ano <= 400)  return 'Século 4';
+   if($ano <= 500)  return 'Século 5';
+   if($ano <= 600)  return 'Século 6';
+   if($ano <= 700)  return 'Século 7';
+   if($ano <= 800)  return 'Século 8';
+   if($ano <= 900)  return 'Século 9';
+   if($ano <= 1000)  return 'Século 10';
+   if($ano <= 1100) return 'Século 11';
+   if($ano <= 1200) return 'Século 12';
+   if($ano <= 1300) return 'Século 13';
+   if($ano <= 1400) return 'Século 14';
+   if($ano <= 1500) return 'Século 15';
+   if($ano <= 1600) return 'Século 16';
+   if($ano <= 1700) return 'Século 17';
+   if($ano <= 1800) return 'Século 18';
+   if($ano <= 1900) return 'Século 19';
+   if($ano <= 2000) return 'Século 20';
+   if($ano <= 2022) return 'Século 21'; 
 
 }
 
